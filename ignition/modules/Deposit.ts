caa59b28e48136c9f891275374887af101086545
brianonchain <jonwayhuang@gmail.com>
2025-07-23 12:37:54 +0800

    sepolia deployed

--- ignition/modules/Deposit.ts
+++ ignition/modules/Deposit.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("DepositModule", (m) => {
-  const constructorArgs = ["0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"]; // usdc address on EthSep
+  const constructorArgs = ["0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"]; // usdc address on Polygon
   const contract = m.contract("Deposit", constructorArgs); // contract instance (called "future" object in hardhat)
   return { contract };
 });
